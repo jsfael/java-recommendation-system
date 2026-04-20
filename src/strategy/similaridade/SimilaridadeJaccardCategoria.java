5bfb4ad226c933f4d83d7895e013a060c09f2b17
jsfael <faeulimao@gmail.com>
2026-04-20 12:45:00 -0300

    feat: implements SimilaridadeJaccardCategoria

---
+++ src/strategy/similaridade/SimilaridadeJaccardCategoria.java
@@ -0,0 +1,36 @@
+package strategy.similaridade;
+
+import model.Usuario;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class SimilaridadeJaccardCategoria implements SimilaridadeStrategy {
+
+    @Override
+    public double calcular(Usuario a, Usuario b) {
+
+        Set<String> categoriasA = a.getItensCurtidos()
+                .stream()
+                .map(item -> item.getCategoria().toLowerCase())
+                .collect(Collectors.toSet());
+
+        Set<String> categoriasB = b.getItensCurtidos()
+                .stream()
+                .map(item -> item.getCategoria().toLowerCase())
+                .collect(Collectors.toSet());
+
+        if (categoriasA.isEmpty() && categoriasB.isEmpty()) {
+            return 0;
+        }
+
+        Set<String> intersecao = new HashSet<>(categoriasA);
+        intersecao.retainAll(categoriasB);
+
+        Set<String> uniao = new HashSet<>(categoriasA);
+        uniao.addAll(categoriasB);
+
+        return (double) intersecao.size() / uniao.size();
+    }
+}
